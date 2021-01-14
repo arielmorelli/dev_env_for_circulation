a986690d16e9a9a551e5f73f91a9f7f1f961b54b
Ariel Morelli <arielmorelli2@gmail.com>
2021-01-14 14:15:09 -0300

    Update base plugin example

--- plugins/base_plugin/cm_plugin_name/__init__.py
+++ plugins/base_plugin/cm_plugin_name/__init__.py
@@ -1,14 +1,6 @@
-from plugin import example_view_func, ExampleScript
+from plugin import Plugin
 
-# A list of routes using from flask add_url_rule syntax
-# https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.add_url_rule
-# A rule always starts with '/'
-routes = [
-    {"rule": "/example", "view_func": example_view_func},
-]
-
-# A list of script to run with the plugin
-scripts = [
-        ExampleScript
-]
+# The __plugin__ must be a class and is necessary to CM undertand how to deal
+# with the plugin
+__plugin__ = Plugin
 
--- plugins/base_plugin/cm_plugin_name/plugin.py
+++ plugins/base_plugin/cm_plugin_name/plugin.py
@@ -1,12 +1,6 @@
 from core.scripts import Script
 
 
-# A route view funcion always will recieve app as argument.
-# Tip: To use the CM DB, just use app.manager._db
-def example_view_func(app):
-    return "example route"
-
-
 # A script should always inherit from Scripts and must have __name__, __frequency__ and run defined
 class ExampleScript(Script):
     __name__ = "example_pluging"
@@ -15,3 +9,22 @@ class ExampleScript(Script):
     def run(self):
         # run don't accept any arguments but self
         print("running")
+
+class Plugin(object):
+    SCRIPTS = [ExampleScript]
+
+    def __init__(self):
+        pass
+
+    def activate(self, app):
+        # Activate function will create flask routes
+        # Tip: To use the CM DB, just use app.manager._db
+
+        @app.route("/new_route")
+        def route_a():
+             return "example route"       
+
+    def run_scripts(self):
+        for script in self.SCRIPTS:
+            script().run()
+
